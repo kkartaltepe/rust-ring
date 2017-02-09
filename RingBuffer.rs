f34b68e03f8b0a2ac7ab3fba65bbe806b5af7313
Kurt Kartaltepe <kkartaltepe@gmail.com>
2017-02-08 21:25:14 -0600

    Try Mutex<Option<u8>>
    This scheme tries to avoid reading head/tail to prevent cache trashing on
    those values, but the mutexes are expensive and so its quite a slow scheme.

--- RingBuffer.rs
+++ RingBuffer.rs
@@ -1,53 +1,61 @@
 use std::sync::{Arc, Mutex, Condvar, MutexGuard};
 use std::result::Result;
-use std::cell::UnsafeCell;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 use std::cmp::{min};
 use std::thread;
 
+#[repr(C)]
 struct RingBuffer {
-	buf: UnsafeCell<Vec<u8>>,
+	buf: Vec<Mutex<Option<u8>>>,
 	cap: usize,
-	used: Mutex<usize>,
+	buf5: i64,
+	buf6: i64,
+	head: AtomicUsize,
+	wlock: Mutex<()>,
+	buf1: i64,
+	buf2: i64,
+	tail: AtomicUsize,
+	rlock: Mutex<()>,
+	buf3: i64,
+	buf4: i64,
+	
+	signal: Mutex<()>,
 	wnotify: Condvar,
 	rnotify: Condvar,
-
-	head: AtomicUsize,
-	tail: AtomicUsize,
-	wlock: Mutex<()>,
-	rlock: Mutex<()>,
 }
+
+const CACHE_BUF: usize = 128;
 
 unsafe impl Sync for RingBuffer {}
 unsafe impl Send for RingBuffer {}
 
-const CACHE_BUF: usize = 128;
-
 impl RingBuffer {
 	pub fn new(capacity: usize) -> RingBuffer {
-		return RingBuffer{
-			buf: UnsafeCell::new(vec![0u8; capacity+CACHE_BUF]),
-			cap: capacity,
-			used: Mutex::new(0),
-			wnotify: Condvar::new(),
-			rnotify: Condvar::new(),
+		let mut inner_buf = Vec::with_capacity(capacity+CACHE_BUF);
+		let mut len = 0;
+		while len <capacity {
+			inner_buf.push(Mutex::new(None));
+			len += 1;
+		}
 
+		return RingBuffer{
+			buf: inner_buf,
+			cap: capacity,
 			head: AtomicUsize::new(0),
 			tail: AtomicUsize::new(0),
 			rlock: Mutex::new(()),
 			wlock: Mutex::new(()),
+			signal: Mutex::new(()),
+			wnotify: Condvar::new(),
+			rnotify: Condvar::new(),
+			buf1: 0,
+			buf2: 0,
+			buf3: 0,
+			buf4: 0,
+			buf5: 0,
+			buf6: 0,
 		};
-	}
-
-	pub fn len(&self) -> usize {
-			let tail = self.tail.load(Ordering::Relaxed);
-			let head = self.head.load(Ordering::Relaxed);
-			let self_buf = unsafe{ &*self.buf.get() };
-			match head >= tail {
-				true => (head - tail),
-				false => ((head + self_buf.len()) - tail),
-			}
 	}
 
 	pub fn read_full(&self, buf: &mut [u8]) -> Result<(), String> {
@@ -58,30 +66,26 @@ impl RingBuffer {
 
 		let to_read = buf.len();
 		let mut have_read = 0;
+		let tail = self.tail.load(Ordering::Acquire);
+		let ref self_buf = self.buf;
+		let buf_len = self_buf.len();
+
 		while have_read < to_read {
-			let mut used = self.used.lock().unwrap();
-			while *used < 1 {
-				used = self.rnotify.wait(used).unwrap();
+			let mut guard = self_buf[(tail+have_read)%buf_len].lock().unwrap();
+			while guard.is_none() {
+				drop(guard);
+				drop(self.rnotify.wait(self.signal.lock().unwrap()).unwrap());
+				guard = self_buf[(tail+have_read)%buf_len].lock().unwrap();
 			}
-			drop(used);
+			
+			buf[have_read] = guard.unwrap();
+			*guard = None;
+			have_read += 1;
 
-			let len = self.len();
-			let tail = self.tail.load(Ordering::Acquire);
-			let self_buf = unsafe{ &*self.buf.get() };
-			let readable = min(len, to_read-have_read);
-			for i in 0..readable {
-				buf[have_read+i] = self_buf[(tail+i)%self_buf.len()];
-			}
-			self.tail.store((tail + readable) % self_buf.len(), Ordering::Release);
-
-			let mut used = self.used.lock().unwrap();
-			*used = self.len();
 			self.wnotify.notify_one();
-			drop(used);
-
-			have_read += readable;
-
 		}
+		self.tail.store((tail+have_read)%self_buf.len(), Ordering::Release);
+
 		drop(rlock);
 		return Ok(());
 	}
@@ -94,31 +98,24 @@ impl RingBuffer {
 
 		let to_write = buf.len();
 		let mut have_write = 0;
+		let head = self.head.load(Ordering::Acquire);
+		let ref self_buf = self.buf;
+		let buf_len = self_buf.len();
 
 		while have_write < to_write {
-			let mut used = self.used.lock().unwrap();
-			while self.cap - *used < 1 {
-				used = self.wnotify.wait(used).unwrap();
+			let mut guard = self_buf[(head+have_write)%buf_len].lock().unwrap();
+			while guard.is_some() {
+				drop(guard);
+				drop(self.wnotify.wait(self.signal.lock().unwrap()).unwrap());
+				guard = self_buf[(head+have_write)%buf_len].lock().unwrap();
 			}
-			drop(used);
+		 	*guard = Some(buf[have_write]);
+			have_write += 1;
 
-			let head = self.head.load(Ordering::Acquire);
-			let self_buf = unsafe{ &mut *self.buf.get() };
-			let buf_len = self_buf.len();
-
-			let free = self.cap - self.len();
-			let writable = min(free, to_write-have_write);
-			for i in 0..writable {
-				 self_buf[(head+i)%buf_len] = buf[have_write+i];
-			}
-			have_write += writable;
-			self.head.store((head + writable) % buf_len, Ordering::Release);
-
-			let mut used = self.used.lock().unwrap();
-			*used = self.len();
 			self.rnotify.notify_one();
-			drop(used);
 		}
+		self.head.store((head+have_write)%buf_len, Ordering::Release);
+
 		drop(wlock);
 		return Ok(());
 	}
@@ -141,7 +138,6 @@ fn main() {
 		threads.push(g);
 	}
 
-	std::thread::sleep(std::time::Duration::from_millis(4000));
 	for _ in 0..10 {
 		let r = ring.clone();
 		let g = thread::spawn(move || {
@@ -162,7 +158,5 @@ fn main() {
 
 	let end = std::time::Instant::now();
 	let dur = end.duration_since(start);
-	let elems = 10*100*1024*1024;
-	println!("{} elems took {}.{} nanos", elems, dur.as_secs(), dur.subsec_nanos());
-	println!("{} elems/s", elems as f64 /(dur.as_secs() as f64 + 0.0000000001f64*(dur.subsec_nanos() as f64)));
+	println!("took {}.{} nanos", dur.as_secs(), dur.subsec_nanos())
 }
